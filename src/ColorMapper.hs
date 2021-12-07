25fbcc88d9b559597516302df54ec6394887fe99
davidjxu21 <63666552+davidjxu21@users.noreply.github.com>
2021-12-06 19:58:51 -0500

    formatting and minor bugfixes

--- src/ColorMapper.hs
+++ src/ColorMapper.hs
@@ -1,14 +1,12 @@
 module ColorMapper where
 
-import LuParser (reserved)
 import Data.Text (Text, pack)
 import qualified Data.Text as T
-
+import LuParser (reserved)
 import Text.Parsec
 import Text.Parsec.Error
 import Text.Parsec.Prim
 import Text.Parsec.Text
-
 
 newtype Color = C {attribute :: String} deriving (Eq, Show)
 
@@ -19,51 +17,54 @@ keyWords = LuParser.reserved
 -- explicit mapping from a string to its Color
 colorMap :: Text -> [(Text, Color)]
 colorMap s = case runParser lineParser () "" s of
-    Left e -> error (concatMap messageString (errorMessages e))
-    Right x -> x
+  Left e -> error (concatMap messageString (errorMessages e))
+  Right x -> x
 
 -- parser for a line
 lineParser :: Stream s m Char => ParsecT s u m [(Text, Color)]
-lineParser =  many1 ( try stringParser <|>
-                      try charParser <|>
-                        try opParser <|>
-                        try wsParser <|>
-                        try keyParser <|>
-                        try miscParser
-                        ) <|> pure [(pack " ", C "text")]
+lineParser =
+  many1
+    ( try stringParser
+        <|> try charParser
+        <|> try opParser
+        <|> try wsParser
+        <|> try keyParser
+        <|> try miscParser
+    )
+    <|> pure [(pack " ", C "text")]
 
 -- parser for spaces
 wsParser :: Stream s m Char => ParsecT s u m (Text, Color)
 wsParser = do
-    x <- many1 (oneOf " \t")
-    return (pack x, C "whitespace")
+  x <- many1 (oneOf " \t")
+  return (pack x, C "whitespace")
 
 -- parses string literals
 stringParser :: Stream s m Char => ParsecT s u m (Text, Color)
 stringParser = do
-    x <- char '\"' *> many (noneOf "'\"\n'") <* char '\"'
-    return (pack ('\"' : x ++ ['\"']), C "string")
+  x <- char '\"' *> many (noneOf "'\"\n'") <* char '\"'
+  return (pack ('\"' : x ++ ['\"']), C "string")
 
 -- parses character literals
 charParser :: Stream s m Char => ParsecT s u m (Text, Color)
 charParser = do
-    x <- char '\'' *> many (noneOf "'\"\n'") <* char '\''
-    return (pack ('\'' : x ++ ['\'']), C "string")
+  x <- char '\'' *> many (noneOf "'\"\n'") <* char '\''
+  return (pack ('\'' : x ++ ['\'']), C "string")
 
 -- parses keywords
 keyParser :: Stream s m Char => ParsecT s u m (Text, Color)
 keyParser = do
-    x <- (choice $ map string ["if", "then", "else", "repeat", "until", "while", "do", "end"]) <* lookAhead (choice [wsParser, pure (T.empty, C $ "") <* eof])
-    return (pack x, C "keyword")
+  x <- (choice $ map (try . string) ["if", "then", "else", "repeat", "until", "while", "do", "end"]) <* lookAhead (choice [wsParser, pure (T.empty, C $ "") <* eof])
+  return (pack x, C "keyword")
 
 -- parses operators
 opParser :: Stream s m Char => ParsecT s u m (Text, Color)
 opParser = do
-    x <- choice $ map string ["+", "-", "*", "/", "%", "=", "<", ">", ".", "not", "{", "}", "[", "]", "#", ";"]
-    return (pack x, C "operator")
+  x <- choice $ map (try . string) ["+", "-", "*", "/", "%", "=", "<", ">", ".", "not", "{", "}", "[", "]", "#", ";"]
+  return (pack x, C "operator")
 
 -- parses everything else
 miscParser :: Stream s m Char => ParsecT s u m (Text, Color)
 miscParser = do
-    x <- many1 (noneOf " +-*{}[]=-><%#/.;\n")
-    return (pack x, C "text")
+  x <- many1 (noneOf " +-*{}[]=-><%#/.;\n")
+  return (pack x, C "text")
--- src/Gui.hs
+++ src/Gui.hs
@@ -1,88 +1,93 @@
-module Gui   where
+module Gui where
 
-
-import System.Exit
+import AutoComplete
 import Brick
 import Brick.AttrMap
 import Brick.Widgets.Border
 import Brick.Widgets.Center
 import Brick.Widgets.Core
-import Cursor.TextField
-import Cursor.List.NonEmpty
-import Cursor.Brick.TextField
-import Cursor.Brick.Text
-import Cursor.Text
-import Cursor.Types
-import LuSyntax hiding (Name)
-import Data.Maybe
-import Formatter
-import System.Environment
 import ColorMapper
-import AutoComplete
-import qualified Graphics.Vty as V
-import Graphics.Vty.Attributes
-import Graphics.Vty.Input.Events 
-import Text.Wrap
-import Path
-import Path.IO
-import Lens.Micro ((^.), (.~), (&), (%~), set)
+import Control.DeepSeq
+import Cursor.Brick.Text
+import Cursor.Brick.TextField
+import Cursor.List.NonEmpty
+import Cursor.Text
+import Cursor.TextField
+import Cursor.Types
+import Data.List.Split (splitOn)
+import Data.Maybe
 import Data.Text (Text)
 import qualified Data.Text as T
 import Data.Text.IO
-import Control.DeepSeq
+import Formatter
+import qualified Graphics.Vty as V
+import Graphics.Vty.Attributes
+import Graphics.Vty.Input.Events
+import Lens.Micro (set, (%~), (&), (.~), (^.))
 import LuParser
-import Data.List.Split (splitOn)
+import LuSyntax hiding (Name)
+import Path
+import Path.IO
+import System.Environment
+import System.Exit
+import Text.Wrap
 
 {--
--- This text editor is heavily inspired by Tom Sydney Kerckhove's tutorial, available at 
+-- This text editor is heavily inspired by Tom Sydney Kerckhove's tutorial, available at
 -- <https://www.youtube.com/watch?v=Kmf3lnln1BI>
 --}
 
 -- | Data model for a text editor, including the string, and dictionary of current words
-data GUI = GUI {
-    cursor :: TextFieldCursor, 
+data GUI = GUI
+  { cursor :: TextFieldCursor,
     dictionary :: [String],
     previous :: Maybe GUI
-}
+  }
 
 -- | Resource Name data type
-data Name = 
-    Text |
-    Viewport
-    deriving (Show, Ord, Eq)
+data Name
+  = Text
+  | Viewport
+  deriving (Show, Ord, Eq)
 
 -- | Application
 app :: App GUI e Name
-app = App {
-    appDraw = drawGUI,
-    appChooseCursor = showFirstCursor,
-    appHandleEvent = handleEvent,
-    appStartEvent = pure,
-    appAttrMap = const $ attrMap mempty [(attrName "text", fg white),
-                                         (attrName "bg", fg black),
-                                         (attrName "keyword", fg blue),
-                                         (attrName "whitespace", fg black),
-                                         (attrName "operator", fg red),
-                                         (attrName "string", fg green)]
-}
+app =
+  App
+    { appDraw = drawGUI,
+      appChooseCursor = showFirstCursor,
+      appHandleEvent = handleEvent,
+      appStartEvent = pure,
+      appAttrMap =
+        const $
+          attrMap
+            mempty
+            [ (attrName "text", fg white),
+              (attrName "bg", fg black),
+              (attrName "keyword", fg blue),
+              (attrName "whitespace", fg black),
+              (attrName "operator", fg red),
+              (attrName "string", fg green)
+            ]
+    }
 
--- handles terminal input. Should take a single argument, 
+-- handles terminal input. Should take a single argument,
 -- and open that as a file
 terminalInit :: IO ()
 terminalInit = do
-    args <- getArgs
-    case args of
-        [x] -> do
-            -- gets the path for a file from the string x
-            path <- resolveFile' x
-            maybeContents <- forgivingAbsence $ Data.Text.IO.readFile (Path.fromAbsFile path)
-            let contents = fromMaybe T.empty maybeContents
-            initialState <- openFile contents
-            endState <- defaultMain app initialState
-            let contents' = rebuildTextFieldCursor (cursor endState)
-            saveFile contents' path
-        -- no arguments passed or more than one argument passed
-        _ -> die "Error: Usage - project-cis552-exe [filename]"
+  args <- getArgs
+  case args of
+    [x] -> do
+      -- gets the path for a file from the string x
+      path <- resolveFile' x
+      maybeContents <- forgivingAbsence $ Data.Text.IO.readFile (Path.fromAbsFile path)
+      let contents = fromMaybe T.empty maybeContents
+      initialState <- openFile contents
+      endState <- defaultMain app initialState
+      let contents' = rebuildTextFieldCursor (cursor endState)
+      saveFile contents' path
+    -- no arguments passed or more than one argument passed
+    _ -> die "Error: Usage - project-cis552-exe [filename]"
 
 -- | saves file and formats it, if it is well-formed
 saveFile :: Text -> Path Abs File -> IO ()
@@ -93,28 +98,28 @@ saveFile txt path = do
       Data.Text.IO.writeFile (Path.fromAbsFile path) txt
     Right block -> Data.Text.IO.writeFile (Path.fromAbsFile path) (formatBlock block)
 
-
 -- initial state, from opening a file to get text
 openFile :: Text -> IO GUI
 openFile text = do
-  return GUI {
-      cursor = makeTextFieldCursor text,
-      dictionary = [],
-      previous = Nothing
-    }
-
+  return
+    GUI
+      { cursor = makeTextFieldCursor text,
+        dictionary = [],
+        previous = Nothing
+      }
 
 -- formats text contained within a GUI, returns a new, formatted GUI
 format :: GUI -> GUI
-format s = let text = rebuildTextFieldCursor (cursor s) in
-  case parseFromText text of
-    Left pe -> s
-    Right block -> let formatted = formatBlock block in
-      s {
-        cursor = makeTextFieldCursor formatted,
-        previous = Just s
-        }
-
+format s =
+  let text = rebuildTextFieldCursor (cursor s)
+   in case parseFromText text of
+        Left pe -> s
+        Right block ->
+          let formatted = formatBlock block
+           in s
+                { cursor = makeTextFieldCursor formatted,
+                  previous = Just s
+                }
 
 -- handles a Brick Event
 handleEvent :: GUI -> BrickEvent Name e -> EventM Name (Next GUI)
@@ -122,8 +127,8 @@ handleEvent s e =
   case e of
     VtyEvent vtye ->
       let mDo ::
-               (TextFieldCursor -> Maybe TextFieldCursor)
-            -> EventM n (Next GUI)
+            (TextFieldCursor -> Maybe TextFieldCursor) ->
+            EventM n (Next GUI)
           mDo f = do
             let curr = cursor s
             let next = fromMaybe curr $ f curr
@@ -140,7 +145,7 @@ handleEvent s e =
             EvKey KEnter [] -> mDo $ Just . textFieldCursorInsertNewline . Just
             EvKey KEsc [] -> halt s -- save and exit
             EvKey (KChar c) [MCtrl] -> case c of
-               -- undo
+              -- undo
               'z' -> continue $ fromMaybe s (previous s)
               -- format
               'f' -> continue $ format s
@@ -152,27 +157,9 @@ handleEvent s e =
             _ -> continue s
     _ -> continue s
 
--- draws the widget for the current line on which the cursor is on. Modified for text wrapping
-selectedTextCursorWidget' :: n -> TextCursor -> Widget n
-selectedTextCursorWidget' n tc = let wgt = createTcWidget tc in
-  Widget Greedy Fixed $ do
-    ctx <- getContext
-    let distance = textCursorIndex tc
-        loc = Brick.Location (distance `mod` ctx^.availWidthL, distance `div` ctx^.availWidthL) in
-      render $ Brick.showCursor n loc wgt
-
--- modified widget helper to wrap lines
-createTcWidget :: TextCursor -> Widget n
-createTcWidget tc =
-  coloredTxtWrap $
-    let t = sanitiseText $ rebuildTextCursor tc
-     in if T.null t
-          then T.pack " "
-          else t
-
 -- modified widget creator to wrap lines
 createTfcWidget :: n -> TextFieldCursor -> Widget n
-createTfcWidget n (TextFieldCursor tfc) = 
+createTfcWidget n (TextFieldCursor tfc) =
   flip foldNonEmptyCursor tfc $ \befores current afters ->
     vBox $
       Prelude.concat
@@ -192,69 +179,80 @@ coloredTxtWrap = txtWrapWith' defaultWrapSettings
 -- modified text wrappers to also take in color
 txtWrapWith' :: WrapSettings -> T.Text -> Widget n
 txtWrapWith' settings s =
-    Widget Greedy Fixed $ do
-      c <- getContext
-      let theLines = fixEmpty <$> wrapTextToLines settings (c^.availWidthL) s
-          fixEmpty l | T.null l = T.pack " "
-                     | otherwise = l
-      case force theLines of
-          [] -> return $ emptyResult & imageL .~ V.text' (c^.attrL) (T.pack " ")
-          multiple ->
-              let maxLength = maximum $ safeTextWidth <$> multiple
-                  padding = V.charFill (c^.attrL) ' ' (c^.availWidthL - maxLength) (length lineImgs)
-                  lineImgs = lineImg <$> multiple
-                  createImage (text, attr) = V.text' 
-                    (attrMapLookup (attrName (attribute attr)) (ctxAttrMap c)) (sanitiseText text)
-                  lineImg lStr = let vals = colorMap lStr
-                                     vals' = map createImage vals
-                                     endspaces = V.text' 
-                                      (c^.attrL)
-                                      (T.replicate (maxLength - safeTextWidth lStr) (T.pack " ")) in
-                      foldr (V.<|>) endspaces vals'
-              in return $ emptyResult & imageL .~ V.horizCat [V.vertCat lineImgs, padding]
-
+  Widget Greedy Fixed $ do
+    c <- getContext
+    let theLines = fixEmpty <$> wrapTextToLines settings (c ^. availWidthL) s
+        fixEmpty l
+          | T.null l = T.pack " "
+          | otherwise = l
+    case force theLines of
+      [] -> return $ emptyResult & imageL .~ V.text' (c ^. attrL) (T.pack " ")
+      multiple ->
+        let maxLength = maximum $ safeTextWidth <$> multiple
+            padding = V.charFill (c ^. attrL) ' ' (c ^. availWidthL - maxLength) (length lineImgs)
+            lineImgs = lineImg <$> multiple
+            createImage (text, attr) =
+              V.text'
+                (attrMapLookup (attrName (attribute attr)) (ctxAttrMap c))
+                (sanitiseText text)
+            lineImg lStr =
+              let vals = colorMap lStr
+                  vals' = map createImage vals
+                  endspaces =
+                    V.text'
+                      (c ^. attrL)
+                      (T.replicate (maxLength - safeTextWidth lStr) (T.pack " "))
+               in foldr (V.<|>) endspaces vals'
+         in return $ emptyResult & imageL .~ V.horizCat [V.vertCat lineImgs, padding]
 
 coloredTxtWrapCursor :: n -> TextCursor -> Widget n
 coloredTxtWrapCursor = txtWrapWithCursor defaultWrapSettings
 
 txtWrapWithCursor :: WrapSettings -> n -> TextCursor -> Widget n
 txtWrapWithCursor settings n tc =
-    Widget Greedy Fixed $ do
-      c <- getContext
-      let s = rebuildTextCursor tc
-          theLines = fixEmpty <$> wrapTextToLines settings (c^.availWidthL) s
-          fixEmpty l | T.null l = T.pack " "
-                     | otherwise = l
-      case force theLines of
-          [] -> let cursorLoc = [CursorLocation (Brick.Location (0, 0)) (Just n)] in
-            return $ set cursorsL cursorLoc (emptyResult & imageL .~ V.text' (c^.attrL) (T.pack " "))
-          multiple ->
-              let maxLength = maximum $ safeTextWidth <$> multiple
-                  loc = findPhysicalLocation tc (safeTextWidth <$> multiple)
-                  cursorLoc = [CursorLocation loc (Just n)]
-                  padding = V.charFill (c^.attrL) ' ' (c^.availWidthL - maxLength) (length lineImgs)
-                  lineImgs = lineImg <$> multiple
-                  createImage (text, attr) = V.text' 
-                    (attrMapLookup (attrName (attribute attr)) (ctxAttrMap c)) (sanitiseText text)
-                  lineImg lStr = let vals = colorMap lStr
-                                     vals' = map createImage vals
-                                     endspaces = V.text' 
-                                      (c^.attrL)
-                                      (T.replicate (maxLength - safeTextWidth lStr) (T.pack " ")) in
-                      foldr (V.<|>) endspaces vals' in
-                  return $ set cursorsL cursorLoc (emptyResult & imageL .~ V.horizCat [V.vertCat lineImgs, padding])
+  Widget Greedy Fixed $ do
+    c <- getContext
+    let s = rebuildTextCursor tc
+        theLines = fixEmpty <$> wrapTextToLines settings (c ^. availWidthL) s
+        fixEmpty l
+          | T.null l = T.pack " "
+          | otherwise = l
+    case force theLines of
+      [] ->
+        let cursorLoc = [CursorLocation (Brick.Location (0, 0)) (Just n)]
+         in return $ set cursorsL cursorLoc (emptyResult & imageL .~ V.text' (c ^. attrL) (T.pack " "))
+      multiple ->
+        let maxLength = maximum $ safeTextWidth <$> multiple
+            loc = findPhysicalLocation tc (safeTextWidth <$> multiple)
+            cursorLoc = [CursorLocation loc (Just n)]
+            padding = V.charFill (c ^. attrL) ' ' (c ^. availWidthL - maxLength) (length lineImgs)
+            lineImgs = lineImg <$> multiple
+            createImage (text, attr) =
+              V.text'
+                (attrMapLookup (attrName (attribute attr)) (ctxAttrMap c))
+                (sanitiseText text)
+            lineImg lStr =
+              let vals = colorMap lStr
+                  vals' = map createImage vals
+                  endspaces =
+                    V.text'
+                      (c ^. attrL)
+                      (T.replicate (maxLength - safeTextWidth lStr) (T.pack " "))
+               in foldr (V.<|>) endspaces vals'
+         in return $ set cursorsL cursorLoc (emptyResult & imageL .~ V.horizCat [V.vertCat lineImgs, padding])
 
 findPhysicalLocation :: TextCursor -> [Int] -> Brick.Location
-findPhysicalLocation tc y = go (textCursorIndex tc) y 0 where
-  go i [] h = Brick.Location (i, h)
-  go i [x] h = if i > (x + 2) then go i [] (h + 1) else Brick.Location (i, h)
-  go i (x : xs) h = if i > x then go (i - x - 1) xs (h + 1) else Brick.Location (i, h)
+findPhysicalLocation tc y = go (textCursorIndex tc) y 0
+  where
+    go i [] h = Brick.Location (i, h)
+    go i [x] h = if i > (x + 2) then go i [] (h + 1) else Brick.Location (i, h)
+    go i (x : xs) h = if i > x then go (i - x - 1) xs (h + 1) else Brick.Location (i, h)
 
 -- draws a GUI
 drawGUI :: GUI -> [Widget Name]
-drawGUI gui = [
-    border $
-    padLeftRight 1 $ viewport Viewport Vertical $ createTfcWidget Text (cursor gui)
+drawGUI gui =
+  [ border $
+      padLeftRight 1 $ viewport Viewport Vertical $ createTfcWidget Text (cursor gui)
   ]
 
 -- gets current (partial) word
